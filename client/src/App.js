31fded2a9babd9d10c75106bff3fbb79b702342b
ssexton1 <ssexton1@uw.edu>
2023-02-21 22:51:45 -0800

    initialize socket-io

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(`http://${window.location.hostname}:3001`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Hello World</p>
       </header>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
---
+++ server/socket.js
@@ -0,0 +1,15 @@
+var { Server } = require("socket.io");
+
+function startSocket(server) {
+	const io = new Server(server, {
+		cors: { origin: "localhost:3000" },
+	});
+	io.on("connection", (socket) => {
+		console.log(`⚡: ${socket.id} user just connected!`);
+		socket.on("disconnect", () => {
+			console.log("🔥: A user disconnected");
+		});
+	});
+}
+
+module.exports = startSocket;
