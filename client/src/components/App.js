e37f789261a1a164d616e3426c53f6a3f243e9c2
Lisette Rinnen <lisette@Lisettes-MacBook-Pro.local>
2023-02-23 16:03:51 -0800

    Create Lobby for host page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,16 @@
 import Onboard from './onboarding';
 import Settings from './settings';
 import CharacterSelect from './createCharacter';
+import LobbyHost from './lobbyHost';
 
 function App() {
   return (
     <div className="App">
 
-      <Onboard />
+      {/* <Onboard />
       <Settings />
-      <CharacterSelect />
+      <CharacterSelect /> */}
+      <LobbyHost />
 
     </div>
   );
