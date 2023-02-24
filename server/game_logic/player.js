87039d28134ce6137dce2c43a37a6d136e6a8706
ssexton1 <63212412+ssexton1@users.noreply.github.com>
2023-02-23 20:37:14 -0800

    start of socket controller and game logic objects

--- server/game_logic/player.js
+++ server/game_logic/player.js
@@ -1,3 +1,6 @@
-class Player {
-  
+module.exports = class Player {
+  constructor(name, character) {
+    this.name = name;
+    this.character = character;
+  }
 }
--- server/game_logic/room.js
+++ server/game_logic/room.js
@@ -1,9 +1,21 @@
-class Room {
-  constructor() {
-    this.playerList = []
+var Settings = require('./settings.js');
+
+module.exports = class Room {
+  constructor(roomCode) {
+    this.roomCode = roomCode;
+    this.playerList = [];
+    this.settings = new Settings();
   }
 
   addPlayer(player) {
     this.playerList.push(player);
+  }
+
+  removePlayer(player) {
+    this.playerList.pop(player);
+  }
+
+  editSettings(activityType, maxDistance, hostLocation) {
+    this.settings.editSettings(activityType, maxDistance, hostLocation);
   }
 }
