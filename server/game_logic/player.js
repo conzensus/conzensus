726537d5a32cdcb7717f1191d496654d88bef5ec
ssexton1 <63212412+ssexton1@users.noreply.github.com>
2023-02-23 20:56:59 -0800

    Revert "Merge branch 'sean/socket-io-basics'"
    This reverts commit 61819aac7663292c7f42a3a6c5cb13e38c206e7d, reversing
    changes made to 67e3e73571fe14e9b334ad24d85228ea7471c613.

--- server/game_logic/player.js
+++ server/game_logic/player.js
@@ -1,6 +1,3 @@
-module.exports = class Player {
-  constructor(name, character) {
-    this.name = name;
-    this.character = character;
-  }
+class Player {
+  
 }
--- server/game_logic/room.js
+++ server/game_logic/room.js
@@ -1,21 +1,9 @@
-var Settings = require('./settings.js');
-
-module.exports = class Room {
-  constructor(roomCode) {
-    this.roomCode = roomCode;
-    this.playerList = [];
-    this.settings = new Settings();
+class Room {
+  constructor() {
+    this.playerList = []
   }
 
   addPlayer(player) {
     this.playerList.push(player);
-  }
-
-  removePlayer(player) {
-    this.playerList.pop(player);
-  }
-
-  editSettings(activityType, maxDistance, hostLocation) {
-    this.settings.editSettings(activityType, maxDistance, hostLocation);
   }
 }
