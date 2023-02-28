115eec68116f42868f5916818a71d44b3c2eea85
Lisette Rinnen <lisette@Lisettes-MacBook-Pro.local>
2023-02-27 19:34:07 -0800

    Add the routers within App.js. Connect all the back and next buttons to ensure staying on the correct path (host or guest)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,26 @@
+import React from 'react';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
+
 import Onboard from './onboarding';
 import Settings from './settings';
-import CharacterSelect from './createCharacter';
+import CharacterSelect from './characterSelect';
 import LobbyHost from './lobbyHost';
 import LobbyGuest from './lobbyGuest';
-
 import GuestJoin from './guestJoin';
 
 function App() {
+
   return (
     <div className="App">
-
-      {/* <Onboard />
-      <Settings />
-      <CharacterSelect />
-      <LobbyHost />
-      <LobbyGuest /> */}
-      <GuestJoin playerInfo={["shiori", "smile.img"]}/>
-
+      <Routes>
+        <Route path="/" element={<Onboard />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/selection" element={<CharacterSelect />} />
+        <Route path="/hostLobby" element={<LobbyHost />} />
+        <Route path="/join" element={<GuestJoin playerInfo={["Jane", "smile.image"]}/>} />
+        <Route path="/guestLobby" element={<LobbyGuest />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/lobbyHost.js
+++ client/src/components/lobbyHost.js
@@ -1,21 +1,34 @@
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function LobbyHost () {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
+  function toStart() {
+    navigate("/");
+  }
+
   return (
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="backBtn" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="backBtn" viewBox="0 0 16 16" onClick={goBack}>
         <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
       </svg>
 
-      <body>
+      <section>
         <h3>Room Code:</h3>
         <input readOnly placeholder="JWXZ"/>
         < AddPlayer />
         <div style={{marginBottom: 100}}>Will add players</div>
-        <button type="button" className="settingBtn">Room Settings</button>
+        <Link to="/settings" state={{ action: "edit" }}>
+          <button type="button" className="settingBtn">Room Settings</button>
+        </Link>
         <br />
-        <button type="button" className="startBtn">Start!</button>
+        <button type="button" className="startBtn" onClick={toStart}>Start!</button>
 
-      </body>
+      </section>
     </div>
   );
 }
