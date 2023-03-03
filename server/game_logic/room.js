5d081d0ef15d6773d52574652d3631bbe68dbbd9
fechan <56131910+fechan@users.noreply.github.com>
2023-03-03 15:38:04 -0800

    add some documentation

--- server/game_logic/room.js
+++ server/game_logic/room.js
@@ -7,10 +7,18 @@ module.exports = class Room {
     this.settings = new Settings(); // Could add host location into settings here if we wanted
   }
 
+  /**
+   * Add a player to the Conzensus room
+   * @param {Player} player Player to add
+   */
   addPlayer(player) {
     this.playerList.push(player);
   }
 
+  /**
+   * Remove a player from the Conzensus room
+   * @param {Player} player Player to remove
+   */
   removePlayer(player) {
     var index = this.playerList.indexOf(player);
     if (index > -1) {
@@ -18,6 +26,12 @@ module.exports = class Room {
     }
   }
 
+  /**
+   * Change the Conzensus room's settings
+   * @param {String} activityType         Type of activity (e.g. outdoor)
+   * @param {Number} maxDistance          Maximum distance from the host location to reccommend activities from
+   * @param {Array<Number>} hostLocation  Latitude and longitude array
+   */
   editSettings(activityType, maxDistance, hostLocation) {
     this.settings.editSettings(activityType, maxDistance, hostLocation);
   }
