f2ee3ae0ecdc65f759b00905caf5a4610479eb2b
fechan <56131910+fechan@users.noreply.github.com>
2023-03-07 16:11:09 -0800

    add test stuff

--- server/tests/game_logic/room.js
+++ server/tests/game_logic/room.js
@@ -2,10 +2,12 @@ const Room = require("../../game_logic/room");
 
 const Player = require("../../game_logic/player");
 const OverpassDataProvider = require("../../data_providers/overpass");
+const FakeTestDataProvider = require("./fake_test_data_provider");
 
 const THE_AVE_COORDS = [47.66009172738974, -122.31304749174053];
 
 const overpass = new OverpassDataProvider();
+const fake_test_data_provider = new FakeTestDataProvider();
 
 
 function roomFirstAddedPlayerBecomesHost() {
@@ -72,8 +74,51 @@ async function roomStartGameReturnsBroadCategories() {
   }
 }
 
+async function roomPlaysEntireRoundSuccessfully() {
+  let room = new Room("TEST", [fake_test_data_provider]);
+  let host = new Player("Alice", "https://picsum.photos/200");
+  room.addPlayer(host);
+  let broadCategories = await room.startGame();
+
+  for (const testCategory of ["test-category-1", "test-category-2", "test-category-3"]) {
+    if (!broadCategories.includes(testCategory)) {
+      console.error(  "Failed test roomPlaysEntireRoundSuccessfully(): " + 
+                      "Room.startGame() doesn't return all broad categories!"  );
+      return;
+    }
+  }
+
+  let filteredActivities = room.filterActivities(["test-category-3"]);
+
+  if (filteredActivities) {
+    if (filteredActivities.length !== 3) {
+      console.error(  "Failed test roomPlaysEntireRoundSuccessfully(): " + 
+                      "Room.filteredActivities() doesn't filter activities correctly!"  );
+      return;
+    }
+  }
+
+  let candidateActivities = room.chooseCandidateActivities(filteredActivities);
+
+  let votes = [
+    {"like": ["test-3.2"], "dislike": ["test-3.1"], "veto": ["test-3.3"]}
+  ];
+  let topActivities = room.getTopActivities(votes, candidateActivities);
+  
+  if (topActivities.length === 3) {
+    console.error(  "Failed test roomPlaysEntireRoundSuccessfully(): " + 
+                    "Room.getTopActivities() returns vetoed activities!"  );
+  } else if (topActivities[0].id == "test-3.1") {
+    console.error(  "Failed test roomPlaysEntireRoundSuccessfully(): " + 
+                    "Room.getTopActivities() returns activities in wrong order!"  );
+  } else {
+    console.log("Passed test roomPlaysEntireRoundSuccessfully()");
+  }
+}
+
 // run tests
 roomFirstAddedPlayerBecomesHost();
 roomSecondPlayerIsNotMadeHost();
 roomNextPlayerIsPromotedOnHostLeave();
 roomStartGameReturnsBroadCategories();
+roomPlaysEntireRoundSuccessfully();
