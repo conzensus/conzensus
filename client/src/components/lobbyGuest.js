44feec4892ee6b6fa87160fdfd03de682f375910
Lisette Rinnen <lisette@Lisettes-MacBook-Pro.local>
2023-03-10 15:47:42 -0800

    Clean redundant code

--- client/src/components/lobbyGuest.js
+++ client/src/components/lobbyGuest.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import AddPlayer from './functions/addPlayer';
+
 export default function LobbyHost({ socket }) {
   const [playerCount, setPlayerCount] = useState(0);
 
@@ -35,37 +37,4 @@ export default function LobbyHost({ socket }) {
       </section>
     </div>
   );
-}
-
-function AddPlayer(response, count) {
-  let placement = document.getElementsByClassName("playerList")[0];
-  let newId = response.playerName + count;
-
-  let newPlayer = document.createElement("div");
-  newPlayer.style.border = "solid";
-  newPlayer.id = newId;
-
-
-  let name = document.createElement("p");
-  name.innerText = response.playerName;
-  newPlayer.appendChild(name);
-
-  // Should be an img
-  let character = document.createElement("p");
-  character.innerText = response.character;
-
-  newPlayer.appendChild(character);
-  placement.appendChild(newPlayer);
-  
-
-  let children = placement.children;
-  for (let i = 0; i < children.length; i++) {
-    console.log(children[i].id);
-    // if (children[i].id != newId) {
-      
-    // }
-  }
-
-  // console.log(placement);
-
 }
--- client/src/components/lobbyHost.js
+++ client/src/components/lobbyHost.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 
+import AddPlayer from "./functions/addPlayer";
+
 export default function LobbyHost ({ socket }) {
   const [playerCount, setPlayerCount] = useState(0);
 
@@ -56,38 +58,4 @@ export default function LobbyHost ({ socket }) {
       </section>
     </div>
   );
-}
-
-function AddPlayer(response, count) {
-  let placement = document.getElementsByClassName("playerList")[0];
-  console.log(placement);
-  let newId = response.playerName + count;
-
-  let newPlayer = document.createElement("div");
-  newPlayer.style.border = "solid";
-  newPlayer.id = newId;
-
-
-  let name = document.createElement("p");
-  name.innerText = response.playerName;
-  newPlayer.appendChild(name);
-
-  // Should be an img
-  let character = document.createElement("p");
-  character.innerText = response.character;
-
-  newPlayer.appendChild(character);
-  placement.appendChild(newPlayer);
-  
-
-  let children = placement.children;
-  for (let i = 0; i < children.length; i++) {
-    console.log(children[i].id);
-    // if (children[i].id != newId) {
-      
-    // }
-  }
-
-  // console.log(placement);
-
 }
